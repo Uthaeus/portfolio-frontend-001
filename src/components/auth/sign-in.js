ea691b2ab7b3a41ebeb0ede05e5bdab0906dfaae
Uthaeus <romanlavery@gmail.com>
2023-06-24 16:47:04 -0600

    built out sign in and sign up forms

--- src/components/auth/sign-in.js
+++ src/components/auth/sign-in.js
@@ -1,8 +1,34 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 
 function SignIn() {
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    function submitHandler(data) {
+        console.log(data);
+        navigate('/');
+    }
+
     return (
-        <div>
-        <h1>Sign In</h1>
+        <div className="signin">
+            <h1>Sign In</h1>
+
+            <form onSubmit={handleSubmit(submitHandler)}>
+                <div className="form-group mb-2">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className="form-control" {...register('email', { required: true })} />
+                    {errors?.email && <span className="error">Email is required</span>}
+                </div>
+
+                <div className="form-group mb-2">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" {...register('password', { required: true })} />
+                    {errors?.password && <span className="error">Password is required</span>}
+                </div>
+
+                <button type="submit" className="btn btn-primary">Sign In</button>
+            </form>
         </div>
     );
 }
--- src/components/auth/sign-up.js
+++ src/components/auth/sign-up.js
@@ -1,8 +1,46 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 
 function SignUp() {
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    function submitHandler(data) {
+        console.log(data);
+        navigate('/');
+    }
+
     return (
-        <div>
-        <h1>Sign Up</h1>
+        <div className="signup">
+            <h1>Sign Up</h1>
+
+            <form onSubmit={handleSubmit(submitHandler)}>
+                <div className="form-group mb-2">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className="form-control" {...register('email', { required: true })} />
+                    {errors?.email && <span className="error">Email is required</span>}
+                </div>
+
+                <div className="form-group mb-2">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" className="form-control" {...register('username', { required: true })} />
+                    {errors?.username && <span className="error">Username is required</span>}
+                </div>
+
+                <div className="form-group mb-2">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" {...register('password', { required: true })} />
+                    {errors?.password && <span className="error">Password is required</span>}
+                </div>
+
+                <div className="form-group mb-2">
+                    <label htmlFor="password_confirmation">Password Confirmation</label>
+                    <input type="password" className="form-control" {...register('password_confirmation', { required: true })} />
+                    {errors?.password_confirmation && <span className="error">Password Confirmation is required</span>}
+                </div>
+
+                <button type="submit" className="btn btn-primary">Sign Up</button>
+            </form>
         </div>
     );
 }
