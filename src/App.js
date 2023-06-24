ebbc14b379f1ed67e424c265e9f4ce5d3c000e43
Uthaeus <romanlavery@gmail.com>
2023-06-24 16:02:46 -0600

    set up initial file structure, created root layout, home, about, contact, components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    
+  }
+]);
+
 function App() {
   return (
     <div>
