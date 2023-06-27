8a15918dc9203ec6bbbec4fd9eec0bd9f7f2f6ae
Uthaeus <romanlavery@gmail.com>
2023-06-27 11:06:19 -0600

    added styling to comment item and form

--- src/components/portfolio/portfolio-comment-item.js
+++ src/components/portfolio/portfolio-comment-item.js
@@ -2,7 +2,7 @@
 function PortfolioCommentItem({ comment }) {
 
     return (
-        <div className="comment-item">
+        <div className="portfolio-comment-item">
             <p className="comment-text">{comment.content}</p>
             <p className="comment-details">posted by: <span className="comment-author">{comment.author}</span></p>
         </div>
