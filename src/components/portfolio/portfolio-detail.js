a7905e535d617b7ffb2f5798413e91a7745a7a8e
Uthaeus <romanlavery@gmail.com>
2023-06-26 21:07:17 -0600

    added styling to portfolio detail

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -25,31 +25,41 @@ function PortfolioDetail() {
     return (
         <div className="portfolio-detail">
             <div className="portfolio-detail-left">
-                <h2 className="portfolio-detail-title">{portfolio.title}</h2>
+                <div className="portfolio-detail-header">
+                    <h2 className="portfolio-detail-title">{portfolio.title}</h2>
+
+                    <div className="portfolio-detail-actions">
+                        {user?.role === 'site_admin' && (
+                            <>
+                                <Link to={`/portfolio/${portfolio.id}/edit`} className="portfolio-detail-action edit-action">Edit</Link>
+                                <Link to={`/portfolio/${portfolio.id}/delete`} className="portfolio-detail-action delete-action">Delete</Link>
+                            </>
+                        )}
+
+                        <Link to='/portfolio' className="portfolio-detail-action back-action">Back</Link>
+                    </div>
+                </div>
 
                 <div className="portfolio-detail-body">
                     <div className="portfolio-detail-body-left">
+                        <div className="portfolio-detail-description-wrapper">
+                            <p className="portfolio-detail-description">{portfolio.description}</p>
+                        </div>
 
+                        <p className="detail-technology-title">technologies used:</p>
+
+                        <div className="detail-technology-wrapper">
+                            {portfolio.technologies.map((technology, index) => <div key={index} className="detail-technology-item">{technology}</div>)}
+                        </div>
                     </div>
 
                     <div className="portfolio-detail-body-right">
-
+                        <img src={`http://localhost:4000/${portfolio.image.url}`} alt="portfolio thumbnail" className="portfolio-detail-image" width='100%' />
                     </div>
                 </div>
-                
-                <div>
-                    {portfolio.technologies.map((technology, index) => <div key={index} className="tech-item">{technology}</div>)}
-                </div>
 
-                <div className="portfolio-detail-actions">
-                    <Link to='/portfolio' className="portfolio-detail-action">Back</Link>
-
-                    {user?.role === 'site_admin' && (
-                        <>
-                            <Link to={`/portfolio/${portfolio.id}/edit`} className="portfolio-detail-action">Edit</Link>
-                            <Link to={`/portfolio/${portfolio.id}/delete`} className="portfolio-detail-action">Delete</Link>
-                        </>
-                    )}
+                <div className="portfolio-detail-comments">
+                    comments here
                 </div>
             </div>
 
