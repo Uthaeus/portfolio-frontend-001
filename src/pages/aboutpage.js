b2093b176f25bb759be6fc5677dcc00b823b17b3
Uthaeus <romanlavery@gmail.com>
2023-06-29 14:44:43 -0600

    imported images and began structuring about page

--- src/pages/aboutpage.js
+++ src/pages/aboutpage.js
@@ -1,9 +1,16 @@
+import { useContext } from "react";
 
+import { UserContext } from "../store/user-context";
 
 function Aboutpage() {
+    const { user } = useContext(UserContext);
+
     return (
         <div className="about-page-container">
-            <h1>About page</h1>
+
+            
+
+            {user?.role === 'site_admin' && <p className="add-skill-btn">add new skill</p>}
         </div>
     )
 }
